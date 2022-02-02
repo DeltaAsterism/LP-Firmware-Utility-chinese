2e4285c4654c4012fda11e8109757634b4b3d17a
ayakaseki <raskisenpai@gmail.com>
2022-02-02 20:22:05 +0800

    webapp dev

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import 'mobx-react-lite/batchingForReactDom'
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import "./assets/main.css";
@@ -10,9 +10,9 @@ import { StoreContext, store } from "./store";
 
 ReactDOM.render(
   <StoreContext.Provider value={store}>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StoreContext.Provider>,
   document.getElementById("root")
 );
